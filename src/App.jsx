ba06b1b8152e6d5bf77cc23f69bf8926a10a17ba
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-15 02:26:38 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import { useState } from 'react'
 import './App.css'
 import Homepage from './components/Page/Homepage/Homepage'
 import Work from './components/Card/Work'
-
+import SampleBooks from './components/Card/SampleBooks'
+import Update from './components/Card/Update'
+import Footer from './components/Footer/Footer'
+import Video from './components/Card/video'
+import Roadmap from './components/Card/Roadmap'
 function App() {
 
 
   return (
     <>
     <Homepage />
+    <SampleBooks />
     <Work />
+    <Video />
+    <Roadmap />
+    <Update />
+    <Footer />
     </>
   )
 }
