c56e8700753e91d4e7645b38378ba04c6fbb102c
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-15 02:27:19 +0530

    a

--- src/components/Page/Homepage/Homepage.jsx
+++ src/components/Page/Homepage/Homepage.jsx
@@ -9,8 +9,7 @@ function Homepage() {
     <div>
 
       <Navbar />
-      <HeroSection />
-      <Features />
+      
     
       <div className="fixed inset-0 -z-10 h-screen w-full bg-[radial-gradient(125%_125%_at_50%_10%,_#000_40%,_#63e_100%)]"></div>
 
