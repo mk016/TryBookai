3dbea080154d9861933113e3dbf6fee2196958a3
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-15 05:24:30 +0530

    add animation

--- src/components/Card/Book.jsx
+++ src/components/Card/Book.jsx
@@ -3,7 +3,7 @@ import book2 from "../../assets/img/book2.png";
 
 function Book() {
   return (
-    <div className="flex items-center justify-center h-100 mt-20 px-4 mb-20">
+    <div className="flex items-center justify-center h-100 mt-20 px-4 mb-20 transform transition-transform duration-300 ease-in-out hover:scale-105">
       <div className="backdrop-blur-sm bg-white/10 py-9 px-9 rounded-xl">
         <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg  xl:max-w-xl">
           <img
