2788d09cf39ed768b83b02c8a89611b2ab6f2b86
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-15 05:24:37 +0530

    add animation

--- src/components/Card/Roadmap.jsx
+++ src/components/Card/Roadmap.jsx
@@ -4,14 +4,14 @@ import { FaUserCircle, FaStore, FaUsers, FaPaintBrush } from "react-icons/fa"; /
 function Roadmap() {
   return (
     <div className="bg-transparent py-8">
-      <div className="backdrop-blur-sm bg-white/10 rounded-lg p-8 sm:p-8 max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto">
+      <div className="backdrop-blur-sm bg-white/10 rounded-lg  p-8 sm:p-8 max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto">
         <div className="text-center mb-8">
           <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 text-2xl sm:text-3xl font-bold">Roadmap</h1>
         </div>
         
-        <div className="flex flex-col grid grid-cols-2">
+        <div className="flex flex-col grid grid-cols-2 sm:grid sm:grid-rows-1 ">
  {/* Roadmap Item 1 */}
- <div className="flex items-center mb-6 sm:mb-8 ">
+ <div className="flex items-center mb-6 sm:mb-8 transform transition-transform duration-300 ease-in-out hover:scale-105 ">
  <div className="text-white text-3xl sm:text-4xl mr-4 bg-gradient-to-r p-3 from-purple-400 to-blue-600 h-[4vh] w-[4vh] rounded-full flex items-center justify-center">
   <FaUserCircle />
 </div>
@@ -27,7 +27,7 @@ function Roadmap() {
         </div>
 
         {/* Roadmap Item 2 */}
-        <div className="flex items-center mb-6 sm:mb-8">
+        <div className="flex items-center mb-6 sm:mb-8 transform transition-transform duration-300 ease-in-out hover:scale-105">
         <div className="text-white text-3xl sm:text-4xl mr-4 bg-gradient-to-r p-3 from-purple-400 to-blue-600 h-[4vh] w-[4vh] rounded-full flex items-center justify-center">
             <FaStore />
           </div>
@@ -42,7 +42,7 @@ function Roadmap() {
         </div>
 
         {/* Roadmap Item 3 */}
-        <div className="flex items-center mb-6 sm:mb-8">
+        <div className="flex items-center mb-6 sm:mb-8 transform transition-transform duration-300 ease-in-out hover:scale-105">
         <div className="text-white text-3xl sm:text-4xl mr-4 bg-gradient-to-r p-3 from-purple-400 to-blue-600 h-[4vh] w-[4vh] rounded-full flex items-center justify-center">
             <FaUsers />
           </div>
@@ -57,7 +57,7 @@ function Roadmap() {
         </div>
 
         {/* Roadmap Item 4 */}
-        <div className="flex items-center mb-6 sm:mb-8">
+        <div className="flex items-center mb-6 sm:mb-8 transform transition-transform duration-300 ease-in-out hover:scale-105">
         <div className="text-white text-3xl sm:text-4xl mr-4 bg-gradient-to-r p-3 from-purple-400 to-blue-600 h-[4vh] w-[4vh] rounded-full flex items-center justify-center">
             <FaPaintBrush />
           </div>
