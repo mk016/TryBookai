91e8e892ce9061d65785e5d2b4de9c75bd9e8b9c
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-15 02:27:12 +0530

    a

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div className='text-center mb-5'>
+            <h3 className='text-white'>@2024 BookAI. All rights reserved</h3>
+        </div>
+        <ul className=' flex justify-around mb-7 text-gray-400'>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
