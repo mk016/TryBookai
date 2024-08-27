8372ed7a7ce467276f127fd70514c8c9ed294576
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-27 22:44:04 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Auth0Provider
+    domain="dev-84fpjbm6ffpt8wdy.us.auth0.com"
+    clientId="XIyL07xIQHnWI6RnaEHkXgZ6XTYK9hpw"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </StrictMode>,
 )
