91c0857a6d28e59e6cddcd2520e38f65d037afae
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-28 22:44:18 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Video from './components/Card/video'
 import Roadmap from './components/Card/Roadmap'
 import UsersSay from './components/Card/UsersSay'
 import AppRoutes from './components/Routes/Routes'
+
 function App() {
 
 
@@ -23,7 +24,7 @@ function App() {
     <UsersSay />
     <Update />
     <Footer />
-
+    <AppRoutes />
     </>
   )
 }
