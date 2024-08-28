92631452f4af1934c874d068ad67f0ec11b8f69d
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-28 22:44:20 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <BrowserRouter>
     <Auth0Provider
     domain="dev-84fpjbm6ffpt8wdy.us.auth0.com"
     clientId="XIyL07xIQHnWI6RnaEHkXgZ6XTYK9hpw"
@@ -13,7 +15,10 @@ createRoot(document.getElementById('root')).render(
       redirect_uri: window.location.origin
     }}
   >
+
     <App />
+
   </Auth0Provider>
+  </BrowserRouter>
   </StrictMode>,
 )
