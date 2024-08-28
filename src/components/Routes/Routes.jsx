9fd7da7c57c74639014cd85f76a5260fe6c23cac
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-28 22:44:30 +0530

    a

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Work from './components/Card/Work';
-import SampleBooks from './components/Card/SampleBooks';
-import Update from './components/Card/Update';
-import Footer from './components/Footer/Footer';
-import Video from './components/Card/Video';
-import Roadmap from './components/Card/Roadmap';
-import UsersSay from './components/Card/UsersSay';
+import { Route, Routes } from 'react-router-dom';
+import Generate from '../BookGenerator/Sidebar';
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sample-books" element={<SampleBooks />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/roadmap" element={<Roadmap />} />
-        <Route path="/users-say" element={<UsersSay />} />
-        <Route path="/update" element={<Update />} />
-        {/* Add more routes as needed */}
-        <Route path="*" element={<div>Page Not Found</div>} /> {/* Fallback route for unknown paths */}
-      </Routes>
-      <Footer />
-    </Router>
+    <Routes>
+      <Route path="/Generate" element={<Generate />} />
+      <Route path="*" element={<div>Page Not Found</div>} />
+    </Routes>
   );
 };
 
