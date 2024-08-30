8406476b270b490c051518f1757959ae6224a5aa
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-31 02:57:53 +0530

    a

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Generate from '../BookGenerator/Sidebar';
+import BookGenerate from '../BookGenerator/BookGenerator';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/Generate" element={<Generate />} />
+      <Route 
+        path="/Generate" 
+        element={
+          <div className='flex'>
+            <Generate />
+            <BookGenerate />
+          </div>
+        } 
+      />
       <Route path="*" element={<div>Page Not Found</div>} />
     </Routes>
   );
